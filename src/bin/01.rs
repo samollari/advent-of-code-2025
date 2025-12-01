204d891a532cc80f7ddc46295171e079ebd2b2a4
Sam Ollari <saollari@wpi.edu>
2025-12-01 01:21:57 -0500

    Day 1 Part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -28,8 +28,29 @@ const fn do_move(dial_pos: u8, mov: &Move) -> u8 {
     .rem_euclid(100) as u8
 }
 
+fn count_move_zero_clicks(dial_pos: u8, mov: &Move) -> u64 {
+    // Count zero clicks:
+    // Right: (start+count)/100
+    // Left: (100-start+count)/100
+
+    (match mov {
+        Move::Right(count) => (dial_pos as u64) + count,
+        Move::Left(count) => {
+            (100 - (if dial_pos == 0 { 100 } else { dial_pos } as i64) + (*count as i64)) as u64
+        }
+    }) / 100
+}
+
+fn parse_input(input: &str) -> Vec<Move> {
+    input
+        .trim()
+        .lines()
+        .map(|line| Move::from_str(line.trim()))
+        .collect()
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
-    let moves = input.trim().lines().map(|line| Move::from_str(line.trim()));
+    let moves = parse_input(input);
     let mut dial_pos = 50;
     let mut zero_count = 0;
     for mov in moves {
@@ -44,7 +65,20 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let moves = parse_input(input);
+    let mut dial_pos = 50;
+    let mut zero_count = 0;
+    for mov in moves {
+        let zero_crossings = count_move_zero_clicks(dial_pos, &mov);
+        let new_pos = do_move(dial_pos, &mov);
+        // println!(
+        //     "{:?}: {} -> {} ({})",
+        //     mov, dial_pos, new_pos, zero_crossings
+        // );
+        dial_pos = new_pos;
+        zero_count += zero_crossings;
+    }
+    Some(zero_count)
 }
 
 #[cfg(test)]
@@ -52,13 +86,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_compute() {
+    fn test_do_move() {
         assert_eq!(do_move(11, &Move::Right(8)), 19);
         assert_eq!(do_move(19, &Move::Left(19)), 0);
         assert_eq!(do_move(0, &Move::Left(1)), 99);
         assert_eq!(do_move(99, &Move::Right(1)), 0);
         assert_eq!(do_move(5, &Move::Left(10)), 95);
         assert_eq!(do_move(95, &Move::Right(5)), 0);
+    }
+
+    #[test]
+    fn test_count_move_zero_clicks() {
+        assert_eq!(count_move_zero_clicks(50, &Move::Left(68)), 1);
+        assert_eq!(count_move_zero_clicks(82, &Move::Left(30)), 0);
+        assert_eq!(count_move_zero_clicks(52, &Move::Right(48)), 1);
     }
 
     #[test]
@@ -70,6 +111,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
