0b908db7da53cee2c6551c031c1a2606e9d05f71
Sam Ollari <saollari@wpi.edu>
2025-12-02 11:17:07 -0500

    Day 2 Part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,32 +1,50 @@
 advent_of_code::solution!(2);
 
-pub fn part_one(input: &str) -> Option<u64> {
-    let range_extents = input
-        .trim()
-        .split(',')
-        .map(|part| part.split_once('-').unwrap())
-        .map(|(left, right)| {
-            (
-                left.parse::<usize>().unwrap(),
-                right.parse::<usize>().unwrap(),
-            )
-        });
-
+fn sum_invalid_ids<F>(input: &str, is_invalid: F) -> Option<u64>
+where
+    F: Fn(&usize) -> bool,
+{
     Some(
-        range_extents
-            .flat_map(|(start, end)| {
-                (start..=end).filter(|val| {
-                    let val = val.to_string();
-                    let (left, right) = val.split_at(val.len() / 2);
-                    *left == *right
-                })
+        input
+            .trim()
+            .split(',')
+            .map(|part| part.split_once('-').unwrap())
+            .map(|(left, right)| {
+                (
+                    left.parse::<usize>().unwrap(),
+                    right.parse::<usize>().unwrap(),
+                )
             })
+            .flat_map(|(start, end)| (start..=end).filter(&is_invalid))
             .sum::<usize>() as u64,
     )
 }
 
+pub fn part_one(input: &str) -> Option<u64> {
+    sum_invalid_ids(input, |val| {
+        let val = val.to_string();
+        let (left, right) = val.split_at(val.len() / 2);
+        *left == *right
+    })
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    sum_invalid_ids(input, |val| {
+        let val = val.to_string();
+        let str_len = val.len();
+        let charvec: Vec<_> = val.chars().collect();
+
+        (2..=str_len).any(|repetition_count| {
+            if str_len % repetition_count != 0 {
+                return false;
+            }
+
+            let chunk_size = str_len / repetition_count;
+            let mut chunk_iter = charvec.chunks(chunk_size);
+            let first_chunk_str: String = chunk_iter.next().unwrap().iter().collect();
+            chunk_iter.all(|chunk| chunk.iter().collect::<String>() == first_chunk_str)
+        })
+    })
 }
 
 #[cfg(test)]
@@ -42,6 +60,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4174379265));
     }
 }
