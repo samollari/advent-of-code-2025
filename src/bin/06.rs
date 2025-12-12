f3814313a896a335dd4b4dd042b26c071bf32217
Sam Ollari <saollari@wpi.edu>
2025-12-11 23:24:34 -0500

    Day 6 Part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -2,12 +2,25 @@ use itertools::Itertools;
 
 advent_of_code::solution!(6);
 
-enum Problem {
-    Addition(Vec<usize>),
-    Multiplication(Vec<usize>),
+#[derive(Debug, Clone, Copy)]
+enum ProblemType {
+    Addition,
+    Multiplication,
 }
 
-fn parse_input(input: &str) -> Vec<Problem> {
+impl ProblemType {
+    fn from(part: &str) -> Option<Self> {
+        match part.trim() {
+            "+" => Some(ProblemType::Addition),
+            "*" => Some(ProblemType::Multiplication),
+            _ => None,
+        }
+    }
+}
+
+type Problem = (ProblemType, Vec<usize>);
+
+fn parse_problems_using_normal_math(input: &str) -> Vec<Problem> {
     let mut parts_by_line = input
         .lines()
         .map(|line| line.trim().split_whitespace().collect_vec())
@@ -19,12 +32,10 @@ fn parse_input(input: &str) -> Vec<Problem> {
         .iter()
         .enumerate()
         .map(|(i, part)| {
-            let operands = get_operands(&parts_by_line, i);
-            match *part {
-                "+" => Problem::Addition(operands),
-                "*" => Problem::Multiplication(operands),
-                _ => panic!(),
-            }
+            (
+                ProblemType::from(part).unwrap(),
+                get_operands(&parts_by_line, i),
+            )
         })
         .collect()
 }
@@ -36,22 +47,81 @@ fn get_operands(parts_by_line: &Vec<Vec<&str>>, i: usize) -> Vec<usize> {
         .collect()
 }
 
-pub fn part_one(input: &str) -> Option<u64> {
-    let problems = parse_input(input);
+fn calc_problems_sum(problems: Vec<Problem>) -> u64 {
+    problems
+        .iter()
+        .map(|(problem_type, values)| match problem_type {
+            ProblemType::Addition => values.iter().sum::<usize>(),
+            ProblemType::Multiplication => values.iter().product(),
+        })
+        .sum::<usize>() as u64
+}
 
-    Some(
-        problems
-            .iter()
-            .map(|problem| match problem {
-                Problem::Addition(items) => items.iter().sum::<usize>(),
-                Problem::Multiplication(items) => items.iter().product(),
-            })
-            .sum::<usize>() as u64,
-    )
+pub fn part_one(input: &str) -> Option<u64> {
+    let problems = parse_problems_using_normal_math(input);
+
+    Some(calc_problems_sum(problems))
+}
+
+fn parse_problem_strings(input: &str) -> Vec<(ProblemType, Vec<&str>)> {
+    let lines = input.lines().collect_vec();
+    let all_space_columns = lines
+        .iter()
+        .map(|line| line.chars().map(|char| char == ' ').collect_vec())
+        .reduce(|acc, x| acc.iter().zip_eq(x).map(|(a, b)| *a && b).collect_vec())
+        .unwrap()
+        .iter()
+        .enumerate()
+        .filter_map(|(i, is_space)| is_space.then_some(i as isize))
+        .collect_vec();
+    let problem_bounds = [-1isize]
+        .iter()
+        .chain(all_space_columns.iter())
+        .chain([lines[0].len() as isize].iter())
+        .tuple_windows()
+        .map(|(lower, upper)| ((*lower + 1) as usize)..(*upper as usize))
+        .collect_vec();
+    problem_bounds
+        .iter()
+        .map(|range| {
+            let mut parts = lines
+                .iter()
+                .map(|line| line.get(range.clone()).unwrap())
+                .collect_vec();
+            let op_type_part = parts.pop().unwrap();
+            (ProblemType::from(op_type_part).unwrap(), parts)
+        })
+        .collect()
+}
+
+fn parse_problems_using_cephalopod_math(input: &str) -> Vec<Problem> {
+    let problem_strings = parse_problem_strings(input);
+
+    problem_strings
+        .iter()
+        .map(|(problem_type, operand_parts)| -> Problem {
+            let mut number_assemblies = vec![vec![]; operand_parts[0].len()];
+
+            for line in operand_parts {
+                for (i, c) in line.chars().enumerate() {
+                    number_assemblies.get_mut(i).unwrap().push(c);
+                }
+            }
+
+            let operands = number_assemblies
+                .iter()
+                .map(|char_vec| char_vec.iter().collect::<String>().trim().parse().unwrap())
+                .collect();
+
+            (*problem_type, operands)
+        })
+        .collect()
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let problems = parse_problems_using_cephalopod_math(input);
+
+    Some(calc_problems_sum(problems))
 }
 
 #[cfg(test)]
@@ -67,6 +137,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3263827));
     }
 }
