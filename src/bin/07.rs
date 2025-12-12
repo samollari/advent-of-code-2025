12dd04eab7b2e252f3de559c173e563f48e00b22
Sam Ollari <saollari@wpi.edu>
2025-12-12 16:05:54 -0500

    Day 7 Part 2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -40,7 +40,39 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut lines = input.lines();
+    let first_line = lines.next().unwrap();
+    let columns = first_line.len();
+    let start_pos = first_line.find('S').unwrap();
+
+    let rest_lines = lines.map(|line| {
+        line.chars().enumerate().filter_map(|(i, c)| match c {
+            '^' => Some(i),
+            _ => None,
+        })
+    });
+
+    let mut timelines = vec![0; columns];
+
+    for line in rest_lines.rev() {
+        for splitter_idx in line {
+            let left_timelines = if splitter_idx > 0 {
+                let val = timelines[splitter_idx - 1];
+                if val != 0 { val } else { 1 }
+            } else {
+                0
+            };
+            let right_timelines = if splitter_idx < columns - 1 {
+                let val = timelines[splitter_idx + 1];
+                if val != 0 { val } else { 1 }
+            } else {
+                0
+            };
+            timelines[splitter_idx] = left_timelines + right_timelines;
+        }
+    }
+
+    Some(timelines[start_pos])
 }
 
 #[cfg(test)]
@@ -56,6 +88,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(40));
     }
 }
