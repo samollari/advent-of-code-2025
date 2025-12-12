5316462f2f67331064a8bf4e2f1c49aae4df8bc6
Sam Ollari <saollari@wpi.edu>
2025-12-12 17:14:21 -0500

    Day 8 Part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -93,7 +93,55 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let coords = parse_input(input);
+
+    let connections = coords
+        .iter()
+        .tuple_combinations()
+        .map(|els| {
+            let (a, b) = els;
+            let (a_x, a_y, a_z) = a;
+            let (b_x, b_y, b_z) = b;
+
+            let dist = ((*a_x as isize - *b_x as isize).pow(2)
+                + (*a_y as isize - *b_y as isize).pow(2)
+                + (*a_z as isize - *b_z as isize).pow(2))
+            .isqrt();
+
+            (els, dist)
+        })
+        .sorted_by(|(_, dist_a), (_, dist_b)| dist_a.cmp(dist_b));
+
+    let mut circuits: Vec<HashSet<&Coord3D>> = vec![];
+
+    let mut last_connected_coords: Option<(&Coord3D, &Coord3D)> = None;
+
+    for ((a_coord, b_coord), _length) in connections {
+        let existing_a_circuit = circuits.iter().find_position(|set| set.contains(a_coord));
+        let existing_b_circuit = circuits.iter().find_position(|set| set.contains(b_coord));
+
+        let both_coords = [a_coord, b_coord];
+
+        match (existing_a_circuit, existing_b_circuit) {
+            (None, None) => circuits.push(HashSet::from(both_coords)),
+            (None, Some((idx, _))) | (Some((idx, _)), None) => circuits[idx].extend(both_coords),
+            (Some((a_idx, _)), Some((b_idx, b_set))) => {
+                if a_idx != b_idx {
+                    let b_set = b_set.clone();
+                    circuits[a_idx].extend(b_set);
+                    circuits[a_idx].extend(both_coords);
+                    circuits.swap_remove(b_idx);
+                }
+            }
+        }
+
+        if circuits.len() == 1 && circuits[0].len() == coords.len() {
+            last_connected_coords = Some((a_coord, b_coord));
+            break;
+        }
+    }
+
+    last_connected_coords.map(|((a_x, _, _), (b_x, _, _))| (a_x * b_x) as u64)
 }
 
 #[cfg(test)]
@@ -109,6 +157,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(25272));
     }
 }
