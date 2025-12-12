2b2726f66b9298dc89cbd81d426260944a4e40b3
Sam Ollari <saollari@wpi.edu>
2025-12-12 00:10:17 -0500

    Day 7 Part 1

---
+++ src/bin/07.rs
@@ -0,0 +1,61 @@
+advent_of_code::solution!(7);
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let mut lines = input.lines();
+    let first_line = lines.next().unwrap();
+    let columns = first_line.len();
+    let start_pos = first_line.find('S').unwrap();
+
+    let rest_lines = lines.map(|line| {
+        line.chars().enumerate().filter_map(|(i, c)| match c {
+            '^' => Some(i),
+            _ => None,
+        })
+    });
+
+    let mut beams = vec![false; columns];
+    beams[start_pos] = true;
+
+    let mut split_count = 0;
+
+    for line in rest_lines {
+        for splitter_idx in line {
+            if !beams[splitter_idx] {
+                continue;
+            }
+
+            split_count += 1;
+
+            beams[splitter_idx] = false;
+            if splitter_idx > 0 {
+                beams[splitter_idx - 1] = true;
+            }
+            if splitter_idx < columns - 1 {
+                beams[splitter_idx + 1] = true;
+            }
+        }
+    }
+
+    Some(split_count)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(21));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
